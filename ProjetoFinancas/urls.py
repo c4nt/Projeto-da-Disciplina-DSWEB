54ad0d97d74bd52a465ac324f6e27b1b8a3acb1f
Marcos <marcosolivee136@gmail.com>
2022-08-11 19:43:24 -0300

    Adição de templates e views

--- ProjetoFinancas/urls.py
+++ ProjetoFinancas/urls.py
@@ -3,8 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index.as_view(), name='index'),
-    path('cadastro/', views.cadastro_usuario.as_view(), name='cadastro'),
+    path('cadastro/', views.cadastro_usuario.as_view(), name='cadastro_usuario'),
     path('despesas/', views.listar_despesas.as_view(), name='listar_despesas'),
+    path('login/', views.login.as_view(), name='login'),
     path('<int:balancete_id>/balancete/', views.gerar_balancete.as_view(), name='gerar_balancete'),
     
 ]
--- ProjetoFinancas/views.py
+++ ProjetoFinancas/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.views import View
 from .models import Despesa, Receita, Balancete, Usuario
+from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class index(View):
@@ -11,7 +13,14 @@ class index(View):
 class cadastro_usuario(View):
     def get(self, request, *args, **kwargs):
         return render(request, 'cadastro.html')
- 
+    def post(self, request, *args, **kwargs):
+        nome = request.POST['nome']
+        senha = request.POST['senha']
+        if nome and senha:
+            user = User.objects.create(username=nome, password=senha)
+            Usuario.objects.create(user=user)
+        return HttpResponseRedirect(reverse('index'))
+
 class listar_despesas(View):
     def get(self, request, *args, **kwargs):
         despesas = Despesa.objects.all()
@@ -21,6 +30,11 @@ class listar_despesas(View):
 #           'balancete': balancete
         }
         return render(request, 'listar_despesas.html', context=context )
+
+class login(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'login.html')
+
 
 class gerar_balancete(View):
     def get(self, request, *args, **kwargs):
