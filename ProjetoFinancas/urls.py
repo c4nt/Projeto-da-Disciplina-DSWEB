acc17ea756f94eaae05a1630d84f6383e6de8cc4
Marcos <marcosolivee136@gmail.com>
2022-08-25 09:43:39 -0300

    Ajustes Finais

--- ProjetoFinancas/urls.py
+++ ProjetoFinancas/urls.py
@@ -3,11 +3,12 @@ from . import views
 
 urlpatterns = [
     path('', views.index.as_view(), name='index'),
-    path('login/', views.login.as_view(), name='login'),
+    path('login/', views.loginView.as_view(), name='login'),
+    path('logout/', views.logoutView, name='logout'),
     path('<int:usuario_id>/dashboard/', views.dashboard.as_view(), name='dashboard'),
-    path('<int:usuario_id>/dashboard/cadastro-contas/', views.cadastro_contas.as_view(), name='cadastro_contas'),
-    path('<int:usuario_id>/dashboard/gerar-balancete/', views.gerar_balancete.as_view(), name='gerar_balancete'),
-    path('<int:usuario_id>/dashboard/arquivo-de-contas/', views.arquivo_de_contas.as_view(), name='arquivo_de_contas'),
+    path('<int:user_id>/dashboard/cadastro-contas/', views.cadastro_contas.as_view(), name='cadastro_contas'),
+    path('<int:user_id>/dashboard/gerar-balancete/', views.gerar_balancete.as_view(), name='gerar_balancete'),
+    path('<int:user_id>/dashboard/arquivo-de-contas/', views.arquivo_de_contas.as_view(), name='arquivo_de_contas'),
     path('cadastro-usuario/', views.cadastro_usuario.as_view(), name='cadastro_usuario'),
     path('despesas/', views.listar_despesas.as_view(), name='listar_despesas'),
 ]
--- ProjetoFinancas/views.py
+++ ProjetoFinancas/views.py
@@ -5,10 +5,13 @@ from django.contrib.auth import login, logout
 from .models import Conta, Usuario
 from django.contrib.auth.models import User
 from django.urls import reverse
-from django.utils.decorators import method_decorator
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, user_passes_test, permission_required
 from datetime import datetime
-
+from django.contrib.auth import login, logout
+from django.contrib.auth import authenticate
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth import get_user_model
+from django.utils.decorators import method_decorator
 
 
 class index(View):
@@ -19,27 +22,27 @@ class cadastro_usuario(View):
     def get(self, request, *args, **kwargs):
         return render(request, 'cadastro_usuario.html')
     def post(self, request, *args, **kwargs):
-        user = User.objects.create(username=request.POST['nome'], password=request.POST['senha'])
-        Usuario.objects.create(user=user, telefone=request.POST['telefone'], email=request.POST['email'])
+        user = User.objects.create_user(username=request.POST['nome'], password=request.POST['senha'])
+        Usuario.objects.create(user=user, id=user.id, telefone=request.POST['telefone'], email=request.POST['email'])
         return HttpResponseRedirect(reverse('login'))
 
+@method_decorator(login_required(login_url='/login'), name='dispatch')
 class cadastro_contas(View):
-    def get(self, request, usuario_id, **kwargs): 
-        usuario = Usuario.objects.get(pk=usuario_id)
+    def get(self, request, user_id, **kwargs): 
+        usuario = User.objects.get(pk=user_id)
         context = {
             'usuario':usuario
         }       
         return render(request, 'cadastro_contas.html', context=context)
         
-    def post(self, request, usuario_id, **kwargs):
-        usuario = Usuario.objects.get(pk=usuario_id)
-        id = usuario.id
+    def post(self, request, *args, **kwargs):
         if request.POST["tipo"] == 'receita':
-            Conta.objects.create(titular=usuario, descricao=request.POST["descricao"], valor=request.POST["valor"], data_criacao=request.POST["data"], ehReceita=True)
+            Conta.objects.create(titular=Usuario.objects.get(pk=request.user.id), descricao=request.POST["descricao"], valor=request.POST["valor"], data_criacao=request.POST["data"], ehReceita=True)
         elif request.POST["tipo"] == 'despesa':
-            Conta.objects.create(titular=usuario, descricao=request.POST["descricao"], valor=request.POST["valor"], data_criacao=request.POST["data"], ehDespesa=True)
-        return HttpResponseRedirect(reverse('dashboard', args=(usuario.id,)))
+            Conta.objects.create(titular=Usuario.objects.get(pk=request.user.id), descricao=request.POST["descricao"], valor=request.POST["valor"], data_criacao=request.POST["data"], ehDespesa=True)
+        return HttpResponseRedirect(reverse('dashboard', args=(request.user.id,)))
 
+@method_decorator(login_required(login_url='/login'), name='dispatch')
 class listar_despesas(View):
     def get(self, request, *args, **kwargs):
         contas = Conta.objects.all().order_by('data_criacao')
@@ -60,10 +63,10 @@ class listar_despesas(View):
         }
         return render(request, 'listar_despesas.html', context=context )
 
+@method_decorator(login_required(login_url='/login'), name='dispatch')
 class gerar_balancete(View):
-    def get(self, request, usuario_id, *args, **kwargs):
-        usuario = Usuario.objects.get(pk=usuario_id)
-        contas = Conta.objects.filter(titular__id = usuario_id, 
+    def get(self, request, *args, **kwargs):
+        contas = Conta.objects.filter(titular__id = request.user.id, 
         data_criacao__month=datetime.today().month).order_by('data_criacao')
         y = 0
         z = 0
@@ -81,20 +84,21 @@ class gerar_balancete(View):
         }
         return render(request, 'gerar_balancete.html', context=context )
 
+@method_decorator(login_required(login_url='/login'), name='dispatch')
 class arquivo_de_contas(View):
-    def get(self, request, usuario_id, **kwargs):
-        usuario = Usuario.objects.get(pk=usuario_id)
+    def get(self, request, user_id, **kwargs):
+        usuario = User.objects.get(pk=user_id)
         context = {
             'usuario':usuario
         }
         return render(request, 'arquivo_de_contas.html', context=context)
 
-    def post(self, request, usuario_id, **kwargs):
-        usuario = Usuario.objects.get(pk=usuario_id)
+    def post(self, request, *args, **kwargs):
+        usuario = User.objects.get(pk=request.user.id)
         m = request.POST['mes']
         if m == '':
             m = 0
-        contas_mes = Conta.objects.filter(titular__id=usuario_id, data_criacao__month=m)
+        contas_mes = Conta.objects.filter(titular__id=request.user.id, data_criacao__month=m)
         msg = "Não exitem contas cadastradas neste período."
         y = 0
         z = 0
@@ -114,25 +118,28 @@ class arquivo_de_contas(View):
         }
         return render(request, 'arquivo_de_contas.html', context=context)
 
-class login(View):
-     def get(self, request, *args, **kwargs):
+class loginView(View):
+    def get(self, request, *args, **kwargs):
         usuarios = Usuario.objects.all()
         return render(request, 'login.html', context = {'usuarios':usuarios})
+    
+    def post(self, request, *args, **kwargs):
+        nome = request.POST['nome']
+        senha = request.POST['senha']
+        user = authenticate(request, username=nome, password=senha)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse('dashboard', args=(user.id,)))
+        else:
+            erro = "Usuário ou senha incorretos"
+            return render(request, 'login.html', context = {'erro':erro})
 
-#    def post(self, request, *args, **kwargs):
- #       nome = request.POST['nome']
-  #      senha = request.POST['senha']
-   #     if user is not None:
-    #        login(request, user)
-     #       if hasattr(request.user, 'cliente'):
-      #          return HttpResponseRedirect(reverse('dashboardcliente'))
-       #     else:
-         #       return HttpResponseRedirect(reverse('fretes_index'))
-        # else:
-       #     erro = 'Email e senha inválidas!'
+def logoutView(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('index'))
 
-#            return render(request, 'login-cadastros/login.html', {'erro': erro})
-
+@method_decorator(login_required(login_url='/login'), name='dispatch')
 class dashboard(View):
     def get(self, request, usuario_id, *args, **kwargs):
-        return render(request, 'dashboard.html')
+        sessao = request
+        return render(request, 'dashboard.html', {'sessao':sessao})
